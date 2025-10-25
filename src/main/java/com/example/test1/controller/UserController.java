fce022a8cc1e3f6892249a73126d58085414b8fb
Lithin87 <72386223+Lithin87@users.noreply.github.com>
2025-10-25 16:20:09 +0530

    added code for lombok

---
+++ src/main/java/com/example/test1/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.test1.controller;
+
+import com.example.test1.model.User;
+import com.example.test1.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+}
